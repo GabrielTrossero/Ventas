865c3d31a3443e92b807ffcc2ee69600213f9f6d
Gabriel Trossero <gabi_trossero@hotmail.com>
2022-11-18 15:59:17 -0300

    generacion de los modelos

---
+++ Backend/WSVenta/Controllers/ClienteController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WSVenta.Models;
+
+namespace WSVenta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using(VentaRealContext db = new VentaRealContext())
+            {
+                var lista = db.Cliente.ToList();
+                return Ok(lista);
+            }
+        }
+    }
+}
---
+++ Backend/WSVenta/Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSVenta.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            List<WeatherForecast> lista = new List<WeatherForecast>();
+            lista.Add(new WeatherForecast() { Id = 5, Name = "Gabriel" });
+            lista.Add(new WeatherForecast() { Id = 6, Name = "Pablo" });
+
+            return lista;
+        }
+    }
+}
